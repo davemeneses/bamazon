8ef8c61820fb57352e080d122733c5114eabd98c
davemeneses <meneses.coffee@gmail.com>
2019-04-03 23:56:17 -0500

    my seeds and sql were not working properly but i fixed it

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+insert into products (product_name, department_name, price, stock_quantity)
+  values ('Nic Cage Pillow Case', 'Furniture', '5.99', 20),
+  ('Grass Flip Flops', 'Apparel', '19.99', 30),
+  ('Ped Egg Callus Remover', 'Appliances', '12.88', 40),
+  ('Madagascar Hissing Cockroaches', 'Pets', '15.50', 10),
+  ('Squatty Potty', 'Furniture', '24.99', 10),
+  ('Giant Gummy Python', 'Grocery', '127.99', 15),
+  ('Mullet Headband', 'Apparel', '9.99', 100),
+  ('Sushi Bazooka', 'Grocery', '14.99', 5),
+  ('Cat Butt Tissue Box', 'Furniture', '34.94', 8),
+  ('Chambong', 'Grocery', '35.00', 25);
