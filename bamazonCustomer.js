e7b660df3a6ea289924b7a4a1e61251d1e339c67
davemeneses <meneses.coffee@gmail.com>
2019-04-03 23:54:26 -0500

    the customer side fully works, could not get validation to work but the app does what its supposed to

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
   user: "root",
-  password: "root",
+  password: "",
   database: "bamazon"
 });
 
@@ -17,25 +17,11 @@ var connection = mysql.createConnection({
 //   console.log("connected as id " + connection.threadId + "\n");
 // });
 
-// this is a function to display the data from MySQUL database
-function displayStock() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
-    console.log("\n");
-    console.table(res);
-    console.log("-----------------------------------------------------");
-    var choiceArr = [];
-    // Loop thru products and add to array
-    for (var j = 0; j < res.length; j++) {
-      // Push products to choice array
-      choiceArr.push(res[j].product_name);
-    }
-  });
-}
+//these are the variable I used to track how much stock there was an any given item
+var stock;
+var updatedStock;
 
-displayStock();
-
-// // this function makes sure the customer is entering a positing number
+// this function makes sure the customer is entering a positing number I couldn't get it to fully work so I just commented it out.
 // function validation(value) {
 //   // Number.isInteger() is cool because it make's sure the customer is entering an integer. parseFloat make a string a number, or in this case an integer
 //   var integer = Number.isInteger(parseFloat(value));
@@ -48,24 +34,102 @@ displayStock();
 //     return "Please enter a positive number.";
 //   }
 // }
-// // this is the function that uses inquirer to help the user make purchases
-// function purchase() {
-//     // this first question asks the user to selection what item they wish to purchase by it's ID number
-//   inquirer.prompt([
-//     {
-//       type: "input",
-//       name: "item_id",
-//       message: "Select the item you wish to purchase by the ID number."
-//       validate: validation,
-//       filter: Number
 
-//     },
-//     {
-//         type: 'input',
-//         name: 'stock_quantity',
-//         message: 'How many would you like to purchase?',
-//         validate: validation,
-//         filter: Number
-//     }
-//   ]);
-// };
+// Customer shopping function
+function userShop() {
+  connection.query(
+    //this tells the computer which table to generate data from the database
+    "SELECT * FROM products",
+    // This displays the table of products the user can choose from.
+    function(err, res) {
+      if (err) throw err;
+      console.log("\n");
+      console.table(res);
+      //   console.log(
+      //     "----------------------------------------------------------------"
+      //   );
+      // This is where I store the user's answers from inquirer.
+
+      var userSelections = [];
+      // Loop thru products and add to array
+      for (var j = 0; j < res.length; j++) {
+        // Push products to choice array
+        userSelections.push(res[j].product_name);
+      }
+      // now we actually start asking the user questions with Inquirer
+      inquirer
+        .prompt([
+          {
+            // this question asks the user to choose an item by it's ID number
+            type: "list",
+            message: "Please select an item to purchase.",
+            name: "itemChoice",
+            choices: userSelections
+          },
+          {
+            // this question asks the user how many of the item they wish to purchase
+            type: "input",
+            message: "Please enter how many you would like to purchase?",
+            name: "itemNum"
+          }
+        ])
+        .then(function(answer) {
+          // this sets the stock number to how many are in the database
+          stock = res[userSelections.indexOf(answer.itemChoice)].stock_quantity;
+          // and this updatedStock is how many are left less the amount the user purchased
+          updatedStock = stock - answer.itemNum;
+          // this if/else stops the user from buying more of the item than there are
+          if (answer.itemNum <= stock) {
+            connection.query(
+              // this updates the number of items in stock based on the user's answers
+              "UPDATE products SET ? WHERE ?",
+              [
+                {
+                  stock_quantity: updatedStock
+                },
+                {
+                  product_name: answer.itemChoice
+                }
+              ],
+              function(err) {
+                if (err) throw err;
+                console.log("Thank you for your purchase!");
+                continueShopping();
+              }
+            );
+            // If there are not enough items left in stock this lets the user know and prompts them to make different selections
+          } else if (answer.itemNum > stock) {
+            console.log("Insufficient stock, please make another selection.");
+            // calls my function that asks the user if they want to keep shopping
+            continueShopping();
+          }
+        });
+    }
+  );
+}
+
+// this calls the userShop().
+userShop();
+
+function continueShopping() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Continue shopping?",
+        name: "continueShopping",
+        choices: ["yes", "no"]
+      }
+    ])
+    .then(function(answer) {
+      if (answer.continueShopping === "yes") {
+        // if the user selects yes the userShop function is called again
+        userShop();
+        // Otherwise...
+      } else {
+        console.log("Thanks, have a great day!");
+        // End the connection
+        connection.end();
+      }
+    });
+}
--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+-- this is the schema
 create database bamazon
 use bamazon
 CREATE TABLE products (
@@ -9,17 +10,17 @@ CREATE TABLE products (
   primary key(id)
 );
 
+-- these are the seeds
 select * from products
 
 insert into products (product_name, department_name, price, stock_quantity)
   values ('Nic Cage Pillow Case', 'Furniture', '5.99', 20),
   ('Grass Flip Flops', 'Apparel', '19.99', 30),
-  ('Nic Cage Pillow Case', 'Furniture', '5.99', 20),
+  ('Ped Egg Callus Remover', 'Appliances', '12.88', 40),
   ('Madagascar Hissing Cockroaches', 'Pets', '15.50', 10),
-  ('Nic Cage Pillow Case', 'Furniture', '5.99', 20),
+  ('Squatty Potty', 'Furniture', '24.99', 20),
   ('Giant Gummy Python', 'Grocery', '127.99', 15),
   ('Mullet Headband', 'Apparel', '9.99', 100),
   ('Sushi Bazooka', 'Grocery', '14.99', 5),
   ('Cat Butt Tissue Box', 'Furniture', '34.94', 8),
   ('Chambong', 'Grocery', '35.00', 25);
-
